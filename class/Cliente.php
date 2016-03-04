4cc21a5d591fb22de7378e708c59192ccf411e63
diego <dieguinho_loko@hotmail.com>
2016-03-04 15:52:55 -0300

    criado a classe Cliente

---
+++ class/Cliente.php
@@ -0,0 +1,103 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: diego
+ * Date: 04/03/16
+ * Time: 10:39
+ */
+class Cliente
+{
+    private $nome;
+    private $idade;
+    private $cpf;
+    private $endereco;
+
+    /**
+     * @return mixed
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * @param mixed $nome
+     */
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIdade()
+    {
+        return $this->idade;
+    }
+
+    /**
+     * @param mixed $idade
+     */
+    public function setIdade($idade)
+    {
+        $this->idade = $idade;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCpf()
+    {
+        return $this->cpf;
+    }
+
+    /**
+     * @param mixed $cpf
+     */
+    public function setCpf($cpf)
+    {
+        $this->cpf = $cpf;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEndereco()
+    {
+        return $this->endereco;
+    }
+
+    /**
+     * @param mixed $endereco
+     */
+    public function setEndereco($endereco)
+    {
+        $this->endereco = $endereco;
+    }
+
+
+    private $indice;
+    private $array;
+
+
+
+    public function __construct($nome,$idade,$cpf,$endereco)
+    {
+        $this->nome = $nome;
+        $this->idade = $idade;
+        $this->cpf = $cpf;
+        $this->endereco = $endereco;
+    }
+
+    public function asc($array)
+    {
+        //us$array;
+    }
+    public function dadosCliente($indice)
+    {
+        $this->setNome($this->array[$indice]->nome);
+    }
+
+}
